4b9ca0b0799029057253fcf538aee5756b09ae12
Darian Tavana <datavana@gmail.com>
2019-04-30 12:50:38 +0000

    finished openTicket method

--- utils/ticketUtils.js
+++ utils/ticketUtils.js
@@ -1,6 +1,6 @@
 module.exports = {
     openTicket: async(client, guild, context) => {
-        context = context.id;
+        let contextid = context.id;
         let channels = await client.provider.get(guild, "ticketchannels", null);
         if(!channels) {
             return `The guild administrators has not setup a ticket channel yet! If you are a guild administrator, please use the \`${guild.commandPrefix}\`command in order to setup the guild.`;
@@ -8,13 +8,27 @@ module.exports = {
 
         channels = JSON.parse(channels);
 
-        if(!(context in channels)) {
-            return `${context.channel.toString()} is not registered as a ticket channel. Use the \`${guild.commandPrefix}channels\` command to see all ticket channels.`
+        let found = false;
+        let targetchannel;
+
+        for(ticketchannel of channels) {
+            if(ticketchannel.channelid === contextid || ticketchannel.contextid === false) {
+                targetchannel = ticketchannel;
+                found = true;
+                break;
+            }
+        }
+
+        if(!found) {
+            return `${context.toString()} was not detected as a Ticket Channel. To view the current ticket channel for this guild, please use the ${guild.commandPrefix}channels command.`;
         }
 
         let currentTicket = await client.provider.get(guild, "currentTicket", null);
         if(!currentTicket) {
             currentTicket = 0;
+        }
+        else {
+            currentTicket = parseInt(currentTicket)
         }
         await client.provider.set(guild, "currentTicket", currentTicket + 1)
         let ticketPrefix = await client.provider.get(guild, "ticketprefix", null);
@@ -22,9 +36,14 @@ module.exports = {
             ticketPrefix = "ticket";
         }
 
-        let adminRole = await guild.roles.fetch(channels.context.adminRole)
-        let moderatorRole = await guild.roles.fetch(channels.context.moderatorRole)
-        let category = await guild.channels.fetch(channels.context.category)
+        let adminRole = await client.provider.get(guild, "adminRole", null);
+        let moderatorRole = await client.provider.get(guild, "moderatorRole", null);
+        if(!adminRole || !moderatorRole) {
+            `The guild administrators has not setup the Ticketer Admin roles. If you are an administrator, please run the ${guild.commandPrefix}setuproles command.`;
+        }
+        adminRole = await guild.roles.get(adminRole);
+        moderatorRole = await guild.roles.get(moderatorRole);
+        let category = await guild.channels.get(ticketchannel.categoryid)
 
         let channel = guild.channels.create(
             `${ticketPrefix}-${currentTicket}`,
@@ -35,7 +54,6 @@ module.exports = {
 
             }
         )
-
         return channel;
     },
     
