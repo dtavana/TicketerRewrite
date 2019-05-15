91c34de592a7a5d3c876e1797a3056c6c606bd6b
Darian Tavana <datavana@gmail.com>
2019-05-15 18:39:44 +0000

    viewsettings command done

---
+++ commands/settings/viewsettings.js
@@ -0,0 +1,54 @@
+const TicketerCommand  = require('../ticketer-command');
+const messageUtils = require('../../utils/messageUtils');
+
+module.exports = class ViewSettingsCommand extends TicketerCommand {
+    constructor(client) {
+        super(client, {
+            name: 'viewsettings',
+            aliases: [],
+            group: 'settings',
+            memberName: 'viewsettings',
+            description: 'Displays current settings for the guild.',
+            guildOnly: true
+        });
+    }
+    
+    async run(msg, fromPattern, result) {
+        let settings = await this.client.provider.getSettings(msg.guild.id);
+        if(!settings || settings.size === 0) {
+            return await messageUtils.sendError({
+                target: msg.channel, 
+                valString: `No settings were detected for **${msg.guild.name}**.`,
+                client: this.client,
+                messages: [msg].concat(result.prompts, result.answers),
+                guild: msg.guild
+            });
+        }
+        let resString = "";
+        let channels = ["logChannel", "transcriptChannel"];
+        let roles = ["moderatorRole", "adminRole"];
+        for(let setting of Object.entries(settings)) {
+            let key = setting[0];
+            let value = setting[1];
+            if(channels.includes(key)) {
+                value = msg.guild.channels.get(value);
+                if(!value) value = "**NOT FOUND**";
+                else value = value.toString();
+            }
+            else if(roles.includes(key)) {
+                value = msg.guild.roles.get(value);
+                if(!value) value = "**NOT FOUND**";
+                else value = value.toString();
+            }
+            else value = `\`${value}\``;
+            resString += `**${key.toUpperCase()}** | ${value}\n`;
+        }
+        await messageUtils.sendSuccess({
+            target: msg.channel, 
+            valString: resString,
+            client: this.client,
+            messages: [msg].concat(result.prompts, result.answers),
+            guild: msg.guild
+        });
+    }
+};
