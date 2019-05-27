bae611157e9b6fd0629b339bc6a78879749d93fa
Darian Tavana <datavana@gmail.com>
2019-05-27 05:01:04 +0000

    upgrade command done

---
+++ commands/info/invite.js
@@ -0,0 +1,25 @@
+const TicketerCommand  = require('../ticketer-command');
+const messageUtils = require('../../utils/messageUtils');
+
+module.exports = class InviteCommand extends TicketerCommand {
+    constructor(client) {
+        super(client, {
+            name: 'invite',
+            aliases: ['inviteme'],
+            group: 'info',
+            memberName: 'invite',
+            description: 'Displays an invite for the bot.'
+        });
+    }
+    
+    async run(msg, fromPattern, result) {
+        let res = `To invite me, [click here](https://discordapp.com/oauth2/authorize?client_id=542709669211275296&scope=bot&permissions=805432400)`;
+        await messageUtils.sendSuccess({
+            target: msg.channel, 
+            valString: res,
+            client: this.client,
+            messages: [msg].concat(result.prompts, result.answers),
+            guild: msg.guild
+        });
+    }
+};
