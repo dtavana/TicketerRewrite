2aab12248c0722cb6cc6f0204c000571fb28788c
Darian Tavana <datavana@gmail.com>
2019-07-25 14:40:17 +0000

    fix posting stats

--- app.js
+++ app.js
@@ -8,12 +8,8 @@ const manager = new ShardingManager('./bot.js', {
     token: process.env.BOT_TOKEN
 });
 
-let posted = false;
-
-manager.on('shardCreate', async(shard) => console.log(`Launched shard ${shard.id}`));
-manager.spawn().then();
-
 const dbl = new DBL(process.env.DBL_TOKEN, {webhookAuth: process.env.DBL_AUTHENTICATION, webhookServer: server});
+
 dbl.webhook.on('ready', hook => {
     console.log(`Webhook running with path ${hook.path}`);
 });
@@ -21,9 +17,20 @@ dbl.webhook.on('vote', async(vote) => {
     await votesController.send(manager, vote);
 });
 
+manager.on('shardCreate', async(shard) => {
+    console.log(`Launched shard ${shard.id}`);
+});
+manager.spawn().then();
+
 server.listen(8080, () => {
     console.log('Webhook server listening');
 });
+
+setInterval(async() => {
+    let guildsArray = await manager.fetchClientValues('guilds.size');
+    let totalGuilds = guildsArray.reduce((prev, guildCount) => prev + guildCount, 0); 
+    await dbl.postStats(totalGuilds);
+}, 900000);
 
 
 
