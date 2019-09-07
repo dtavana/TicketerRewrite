383b4ad7569b0ca23681571d8d209c63458885e8
Darian Tavana <datavana@gmail.com>
2019-09-07 18:04:48 +0000

    fix donation stuff

--- controllers/donate.controller.js
+++ controllers/donate.controller.js
@@ -5,7 +5,13 @@ const pg = require('./postgres.controller');
 
 module.exports = {
     send: async(manager, data) => {
-        const { status, buyer_id: userId, txn_id: paymentId } = data;
+        const { status, txn_id: paymentId } = data;
+        let userId = data.buyer_id;
+        const prefixText = "nonrole:";
+        const rolePrefix = userId.indexOf(prefixText);
+        if(rolePrefix !== -1) {
+            userId = userId.substring(prefixText.length)
+        }
         let added = true;
         let key;
         if(status === "completed") {
