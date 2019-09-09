569fcd567b1b662f03d7e542dd3437ed29ee3e83
Darian Tavana <datavana@gmail.com>
2019-09-09 19:06:42 +0000

    seperate non important vars from .env

---
+++ utils/configToProcess.js
@@ -0,0 +1,10 @@
+const config = require("../config.json");
+
+const configToProcess = () => {
+    for(let entry of Object.entries(config)) {
+        console.log(`Assigning process.env.${entry[0]} to ${entry[1]}`)
+        process.env[entry[0]] = entry[1];
+    }
+}
+
+module.exports = configToProcess();
