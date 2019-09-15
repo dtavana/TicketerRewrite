2c6392300721c32e9128885c91243fab8d4680cd
Darian Tavana <datavana@gmail.com>
2019-09-15 19:23:11 +0000

    lint

--- utils/configToProcess.js
+++ utils/configToProcess.js
@@ -1,10 +1,10 @@
-const config = require("../config.json");
+const config = require('../config.json');
 
 const configToProcess = () => {
     for(let entry of Object.entries(config)) {
-        console.log(`Assigning process.env.${entry[0]} to ${entry[1]}`)
+        console.log(`Assigning process.env.${entry[0]} to ${entry[1]}`);
         process.env[entry[0]] = entry[1];
     }
-}
+};
 
 module.exports = configToProcess();
